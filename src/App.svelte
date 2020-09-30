6ec393ce0d044820da9fd16888049a750e488d6b
Tyler Rick <tyler@tylerrick.com>
2020-09-30 12:55:20 -0700

    Same result with version of components from svelte-final-form

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script>
-import { FieldArray } from "my-new-component";
-// import { FieldArray } from "svelte-final-form";
+// import { FieldArray } from "my-new-component";
+import { FieldArray } from "svelte-final-form";
 
-import { UsesContext } from "my-new-component";
-// import { UsesContext } from "svelte-final-form";
+// import { UsesContext } from "my-new-component";
+import { UsesContext } from "svelte-final-form";
 
 </script>
 
