849f3beb67fa2cd390c2adfbec53027524e070ff
Tyler Rick <tyler@tylerrick.com>
2020-09-30 21:51:16 -0700

    Prove that using local identical copy of UsesContext does *not* cause error

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,12 @@
 <script>
 import { FieldArray } from "my-new-component";
-import { UsesContext } from "my-new-component";
+
+// Using this results in error:
+//   Uncaught (in promise) Error: Function called outside component initialization
+// import { UsesContext } from "my-new-component";
+
+// But changing it to use a copy of the exact some component located in local app works without any error:
+import UsesContext from "./UsesContext.svelte";
 </script>
 
 <FieldArray let:names>
