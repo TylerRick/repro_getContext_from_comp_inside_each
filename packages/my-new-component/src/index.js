324c29d192154340d473739dc60caba0735dac12
Tyler Rick <tyler@tylerrick.com>
2020-09-30 22:39:26 -0700

    Revert "Change source to packages/my-new-component as part of this monorepo to make it even easier to hack"
    https://codesandbox.io/s/github/TylerRick/repro_getContext_from_comp_inside_each files to find local
    package:
    
      Could not find file:packages/my-new-component while resolving dependency

--- packages/my-new-component/src/index.js
+++
@@ -1,5 +0,0 @@
-export { default as NewComp } from './Component.svelte';
-
-export { default as FieldArray } from './FieldArray.svelte'
-export { default as UsesContext } from './UsesContext.svelte'
-
